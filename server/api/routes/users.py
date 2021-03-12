1692cc093fc3b837035722201635019dd0dbe7c4
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2021-03-11 20:15:58 -0600

    Creates registration route that creates a new user and returns the data

--- server/api/routes/users.py
+++ server/api/routes/users.py
@@ -16,3 +16,22 @@ def login():
     req = request.get_json()
     user = User.objects(email=req['email'], password=req['password'])
     return str(bool(user))
+
+
+@users.route('/register', methods=['POST'])
+def register():
+    """POST users/register
+
+    Returns:
+        email: email of created user
+        password: password of created user
+
+    Status:
+        201: new user successfully created
+    """
+    req = request.get_json()
+    new_user = User(email=req['email'], password=req['password']).save()
+    return {
+        'email': new_user.email,
+        'password': new_user.password
+    }, 201
