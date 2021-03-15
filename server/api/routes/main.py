2954439420436657a2192298ad999fa439ad05e2
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2021-03-15 12:47:20 -0500

    Creates protected route to test functionality of the jwt authorization

--- server/api/routes/main.py
+++ server/api/routes/main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from flask_jwt_extended import get_jwt_identity, jwt_required
 from database import User
 import json
 
@@ -8,3 +9,10 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return "Hello, world"
+
+
+@main.route('/protected')
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return current_user
