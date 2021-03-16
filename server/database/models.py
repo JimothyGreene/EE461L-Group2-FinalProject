33c5468b54a2f72b367fe083182bea81c8aadbac
Rishi Ponnekanti <51857775+JimothyGreene@users.noreply.github.com>
2021-03-15 20:39:01 -0500

    Update User Model (#39)

--- server/database/models.py
+++ server/database/models.py
@@ -5,6 +5,16 @@ db = MongoEngine()
 
 
 class User(db.Document):
-    # Once seeding issues figured out, add unique=True
+    """User for our site. Data used to log into and manage
+    resources for the site
+
+    Fields:
+        first_name: user's first name
+        last_name: user's last name
+        email: user's email (unique identifier)
+        password: user's hashed password
+    """
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
     email = db.EmailField(required=True, unique=True)
-    password = db.StringField(require=True)
+    password = db.StringField(required=True)
