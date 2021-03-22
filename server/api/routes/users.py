da32302c4c98e57623b3a8b59bb3ddc2254ecb2a
ishan0102 <ishan0102@gmail.com>
2021-03-22 14:06:15 -0500

    Restructure validation by abstracting errors

--- server/api/routes/users.py
+++ server/api/routes/users.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request
 from flask_jwt_extended import create_access_token
 from database import User
-from api.routes.validators.users import valid_email, valid_password
+from api.routes.validators.users import valid_login, valid_registration
 
 users = Blueprint('users', __name__)
 
@@ -10,36 +10,26 @@ def login():
     """POST users/login
 
     Desc: Logs in a user
-          Ensures fields are not blank
-          Validates email used to log in
+          Checks if any fields are blank
           Checks if an account exists at all
           Checks if an account exists and the password was incorrect
 
     Returns:
         200: success - true
         401: password was incorrect, unauthorized access
-        404: no account was found
+        404: no account was found or field was empty
     """
     req = request.get_json()
-    if req['email'] == '' or req['password'] == '':
-        return {
-            'error': 'Fields must not be empty.'
-        }, 404
+    is_valid, errors = valid_login(req)
+    if not is_valid:
+        return errors, 404
 
-    check_email = valid_email(req['email'])
-    if not check_email[0]:
-        return {
-            'error': check_email[1]
-        }, 401
-    
     user = User.objects(email=req['email']).first()
     if not user:
         return {
             'error': 'No account with this email exists.'
         }, 404
-
-    user = User.objects(email=req['email'], password=req['password']).first()
-    if not user:
+    if user.password != req['password']:
         return {
             'error': 'An account was found but the password is incorrect.'
         }, 401
@@ -48,7 +38,6 @@ def login():
     return {
         'token': access_token
     }, 200
-
 
 @users.route('/register', methods=['POST'])
 def register():
@@ -64,19 +53,11 @@ def register():
         409: duplicate account, conflict with database
     """
     req = request.get_json()
-    check_email = valid_email(req['email'])
-    check_password = valid_password(req['password'])
-
-    if not check_email[0]:
-        return {
-            'error': check_email[1]
-        }, 401
-    if not check_password[0]:
-        return {
-            'error': check_password[1]
-        }, 401
+    is_valid, errors = valid_registration(req)
+    if not is_valid:
+        return errors, 401
     
-    user = User.objects(email=check_email[1]).first()
+    user = User.objects(email=req['email']).first()
     if user:
         return {
             'error': 'Account could not be created. User already exists.'
--- server/api/routes/validators/users.py
+++ server/api/routes/validators/users.py
@@ -1,45 +1,98 @@
 from email_validator import validate_email, EmailNotValidError
 
-def valid_email(email):
+def check_email(email):
     """Checks if an email is valid
 
     Args:
         email (str): user's email address
 
     Returns:
-        (True, email): normalized email if it is valid
+        (True, ''): normalized email if it is valid
         (False, error): error message if it is invalid
     """
     try:
         valid = validate_email(email)
         email = valid.email
     except EmailNotValidError as e:
-        return (False, str(e))
+        return False, str(e)
 
-    return (True, email)
+    return True, ''
 
-def valid_password(password):
+def check_password(password):
     """Checks if a password is valid
 
     Args:
         password (string): user's password
 
     Returns:
-        (True, password): password if it is valid
+        (True, ''): password if it is valid
         (False, error): error message if it is invalid
     """
 
     # Is at least 8 characters long
     if len(password) < 8:
-        return (False, 'The password is not valid. It must have at least 8 characters.')
+        return False, 'Password must have at least 8 characters.'
     # Contains a number
     if not any(c.isdigit() for c in password):
-        return (False, 'The password is not valid. It must contains at least 1 number.')
+        return False, 'Password must contain a number.'
     # Contains an uppercase character
     if not any(c.isupper() for c in password):
-        return (False, 'The password is not valid. It must contains at least 1 uppercase character.')
+        return False, 'Password must contain an uppercase character.'
     # Contains a lowercase character
     if not any(c.islower() for c in password):
-        return (False, 'The password is not valid. It must contains at least 1 lowercase character.')
+        return False, 'Password must contain a lowercase character.'
 
-    return (True, password)
+    return True, ''
+
+def valid_login(req):
+    """Checks if login is valid
+
+    Args:
+        req (JSON): contains email and password strings
+
+    Returns:
+        is_valid: whether the email and password are valid
+        errors: dictionary of email and/or password errors
+    """
+    errors = {
+        'email': '',
+        'password': ''
+    }
+    
+    is_valid = True
+    if req['email'] == '':
+        is_valid = False
+        errors['email'] = 'Email must not be left blank'
+    if req['password'] == '':
+        is_valid = False
+        errors['password'] = 'Password must not be left blank'
+    
+    return is_valid, errors
+
+def valid_registration(req):
+    """Checks if registration is valid
+
+    Args:
+        req (JSON): contains email and password strings
+
+    Returns:
+        is_valid: whether the email and password are valid
+        errors: dictionary of email and/or password errors
+    """
+    errors = {
+        'email': '',
+        'password': ''
+    }
+
+    is_valid_email, email_error = check_email(req['email'])
+    is_valid_password, password_error = check_password(req['password'])
+    
+    is_valid = True
+    if not is_valid_email:
+        is_valid = False
+        errors['email'] = email_error
+    if not is_valid_password:
+        is_valid = False
+        errors['password'] = password_error
+    
+    return is_valid, errors
