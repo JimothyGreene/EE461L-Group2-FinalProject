59a3b79ba8663dd299b3c50c0353b119a27095ed
Xige Michael Chen <mxchen2001@gmail.com>
2021-03-23 09:49:27 -0500

    removed API functions

--- server/api/routes/projects.py
+++ server/api/routes/projects.py
@@ -6,22 +6,3 @@ import json
 
 projects = Blueprint('projects', __name__)
 
-
-@projects.route('/create_projects', methods=['POST'])
-def create_projects():
-    req = request.get_json()
-    new_proj = Projects(**req).save()
-    return new_proj.to_json(), 200
-
-@projects.route('/read_projects')
-def read_projects():
-
-    return
-@projects.route('/update_projects')
-def update_projects():
-
-    return
-@projects.route('/delete_projects')
-def delete_projects():
-
-    return
