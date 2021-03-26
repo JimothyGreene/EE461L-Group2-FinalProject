b2374b751ace346a1f5a33e9b03a6618ed579156
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2021-03-26 16:58:53 -0500

    Modifies API to use a MockMongoDB when testing

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,8 +1,15 @@
 import pytest
+import os
 from api import create_app
 
 
+class TestConfig:
+    MONGODB_SETTINGS = {
+        'host': 'mongomock://localhost',
+    }
+    JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
+
 @pytest.fixture
 def app():
-    app = create_app()
+    app = create_app(TestConfig)
     return app
--- server/tests/test_users.py
+++ server/tests/test_users.py
@@ -3,7 +3,7 @@
 This includes registration and login
 """
 
-def test_register(client):
+def test_register(client, mongodb):
     # Successful registration
     res = client.post('/users/register', json={
         "first_name": "FirstName",
@@ -12,3 +12,13 @@ def test_register(client):
         "password": "P@ssw0rd!"
     })
     assert res.status_code == 201
+
+
+def test_login(client, mongodb):
+    # Successful login
+    res = client.post('/users/login', json={
+        "email": "testemail@domain.com",
+        "password": "T3stP@ssword"
+    })
+    print(res.data)
+    assert res.status_code == 200
