1bc747513d692208f20410973f954cfdea35a039
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2021-03-27 00:00:02 -0500

    Trying to use GitHub auto-set env variables to check

--- server/api/config.py
+++ server/api/config.py
@@ -3,10 +3,10 @@ import os
 
 class Config:
     MONGODB_SETTINGS = {
-        'username': os.environ['MONGODB_USERNAME'] or None,
-        'password': os.environ['MONGODB_PASSWORD'] or None,
-        'db': os.environ['MONGODB_DATABASE'] or None,
-        'host': os.environ['MONGODB_HOST'] or None,
+        'username': os.environ['MONGODB_USERNAME'],
+        'password': os.environ['MONGODB_PASSWORD'],
+        'db': os.environ['MONGODB_DATABASE'],
+        'host': os.environ['MONGODB_HOST'],
         'authentication_source': 'admin'
-    }
+    } if os.environ['CI'] not in ['True', 'true'] else {}
     JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
