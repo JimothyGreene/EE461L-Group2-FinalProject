6b1f44baecee95ef40987fab1e55bb30a6db97f7
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2021-03-26 23:11:50 -0500

    Pytest is functioning with a mongomock database

--- server/tests/test_users.py
+++ server/tests/test_users.py
@@ -2,23 +2,35 @@
 
 This includes registration and login
 """
+from database import User
+import json
 
-def test_register(client, mongodb):
+def test_register(client):
     # Successful registration
-    res = client.post('/users/register', json={
-        "first_name": "FirstName",
-        "last_name": "LastName",
-        "email": "firstnamelastname@domain.com",
-        "password": "P@ssw0rd!"
-    })
-    assert res.status_code == 201
-
-
-def test_login(client, mongodb):
-    # Successful login
-    res = client.post('/users/login', json={
+    user_data = {
+        "first_name": "TestFirstName",
+        "last_name": "TestLastName",
         "email": "testemail@domain.com",
         "password": "T3stP@ssword"
+    }
+    res = client.post('/users/register', json=user_data)
+    assert res.status_code == 201
+    new_user = json.loads(res.data)
+    for key in user_data:
+        assert user_data[key] == new_user[key]
+
+
+def test_login(client):
+    # Successful login
+    user_data = {
+        "first_name": "TestFirstName",
+        "last_name": "TestLastName",
+        "email": "testemail@domain.com",
+        "password": "T3stP@ssword"
+    }
+    User(**user_data).save()
+    res = client.post('/users/login', json={
+        "email": user_data["email"],
+        "password": user_data["password"]
     })
-    print(res.data)
     assert res.status_code == 200
