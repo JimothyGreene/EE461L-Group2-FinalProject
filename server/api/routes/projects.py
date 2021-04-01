969d002b0cda65d9054dd764efba3be9fab2fc3f
Xige Michael Chen <mxchen2001@gmail.com>
2021-04-01 17:51:40 -0500

    Basic CRUD for projects (#59)
    * Basic CRUD for projects
    
    * user jwt for creator identity
    
    * swagger documentation
    
    * associate READ project with current user
    
    * updated documentation

--- server/api/routes/projects.py
+++ server/api/routes/projects.py
@@ -1,8 +1,58 @@
 from flask import Blueprint, request
 from database import Projects
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity, jwt_required
 # from database import User
 import json
 
 projects = Blueprint('projects', __name__)
 
+@projects.route('/', methods=['POST'])
+@jwt_required()
+def projects_create():
+    """POST projects/
+    Desc: Creates a new project
+    Returns:
+        201: newly created project
+    """
+    req = request.get_json()
+    req["creator_id"] = get_jwt_identity()["_id"]["$oid"]
+    new_project = Projects(**req).save()
+    return new_project.to_json(), 201
+
+
+@projects.route('/', methods=['GET'])
+@jwt_required()
+def projects_read():
+    """GET projects/
+    Desc: Gets all available projects associated with the current user
+    Returns:
+        200: all projects in the database
+    """
+    return Projects.objects(creator_id=get_jwt_identity()["_id"]["$oid"]).to_json(), 200
+
+
+
+@projects.route('/<id>', methods=['PUT'])
+@jwt_required()
+def projects_update(id):
+    """PUT projects/<id>
+    Desc: Updates a specific projects
+    Returns:
+        200: updated projects object
+    """
+    req = request.get_json()
+    curr_project = Projects.objects(id=id).first()
+    curr_project.update(**req)
+    curr_project.reload()
+    return curr_project.to_json(), 200
+
+@projects.route('/<id>', methods=['DELETE'])
+@jwt_required()
+def projects_delete(id):
+    """DELETE projects/<id>
+    Desc: Deletes a specific project from the database
+    Returns:
+        200: success message
+    """
+    Projects.objects(id=id).delete()
+    return {'msg': 'Projects set successfully deleted'}, 200
