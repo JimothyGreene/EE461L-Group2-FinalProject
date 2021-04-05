69f96c981cda1ab5901369173ab59fc0e7ee9b06
maxwithrow <maxwithrow@utexas.edu>
2021-04-05 01:20:48 -0500

    updating docker-compose and api.js

---
+++ client/src/util/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: process.env.REACT_APP_PROD ? process.env.REACT_APP_API_URL : 'http://localhost:5000/', 
+    responseType: 'json',
+    auth: localStorage.getItem("token") !== null ? `Bearer ${localStorage.getItem("token").replace(/['"]+/g, '')}` : null
+});
+
+export default api;
