1b6b35b6801e24c483cf00dc625fcbd82bb16d33
maxwithrow <maxwithrow@utexas.edu>
2021-04-07 14:30:25 -0500

    cleaning up

--- client/src/components/DatasetCard.js
+++ client/src/components/DatasetCard.js
@@ -4,7 +4,6 @@ import { NavLink } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 import {Grid, List, ListItem, ListItemText} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
-import { NavLink } from 'react-router-dom';
 
 
 function preventDefault(event) {
