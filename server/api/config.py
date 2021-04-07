d0a325b8b3ece626d22097f39a630904e5ee6b3a
Rishi Ponnekanti <51857775+JimothyGreene@users.noreply.github.com>
2021-04-06 22:40:33 -0500

    Fixes configuration KeyError due to CI setup (#90)

--- server/api/config.py
+++ server/api/config.py
@@ -1,13 +1,20 @@
 import os
 import datetime
 
+
 class Config:
-    MONGODB_SETTINGS = {
-        'username': os.environ['MONGODB_USERNAME'],
-        'password': os.environ['MONGODB_PASSWORD'],
-        'db': os.environ['MONGODB_DATABASE'],
-        'host': os.environ['MONGODB_HOST'],
-        'authentication_source': 'admin'
-    } if os.environ['CI'] not in ['True', 'true'] else {}
-    JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
-    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=14)
+    MONGODB_SETTINGS = {}
+    JWT_SECRET_KEY = {}
+    JWT_ACCESS_TOKEN_EXPIRES = {}
+    try:
+        MONGODB_SETTINGS = {
+            'username': os.environ['MONGODB_USERNAME'],
+            'password': os.environ['MONGODB_PASSWORD'],
+            'db': os.environ['MONGODB_DATABASE'],
+            'host': os.environ['MONGODB_HOST'],
+            'authentication_source': 'admin'
+        } if os.environ['CI'] not in ['True', 'true'] else {}
+        JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
+        JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=14)
+    except KeyError:
+        pass
