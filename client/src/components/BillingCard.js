353c46a40d7d97074a4a7eb10975035c6dfb48e7
Xige Michael Chen <mxchen2001@gmail.com>
2021-05-03 22:39:36 -0500

    fix conflict  2

--- client/src/components/BillingCard.js
+++ client/src/components/BillingCard.js
@@ -5,17 +5,12 @@ import Link from '@material-ui/core/Link';
 import { NavLink } from 'react-router-dom'
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-<<<<<<< HEAD
 import api from '../util/api';
 import { AuthContext } from './AuthContext';
 import { makeStyles } from '@material-ui/core/styles';
 
 class BillingCard extends React.Component {
     static contextType = AuthContext;
-=======
-import { NavLink } from 'react-router-dom';
-import api from '../util/api';
->>>>>>> 818812ec2db88f28e8e8d7bbdb0869342307964d
 
     constructor(props) { 
         super(props);
@@ -38,7 +33,6 @@ import api from '../util/api';
 
     }
 
-<<<<<<< HEAD
     getProjects() {
         if(this.state.currProjectID !== null && this.state.currProjectID !== undefined) {
             api().get(`projects/${this.state.currProjectID.replace(/['"]+/g, '')}`)
@@ -133,102 +127,3 @@ import api from '../util/api';
 }
 
 export default BillingCard;
-=======
-export default class BillingCard extends React.Component{
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            currentProjectHardware: []
-        }
-    }
-
-    componentDidMount() {
-        console.log("testing")
-        api().get("/projects/")
-                .then((res) => {
-                    console.log(res)
-                    this.setState({
-                        currProjectHardware: res.data.hardware
-                    });
-                })
-                .catch((e) => {
-                    console.log("NOTWORKING", e)
-                    if(e.response !== null && e.response !== undefined) {
-                        this.setState({
-                            joinError: true,
-                            joinHelperText: e.response.data.msg.replace(/['"]+/g, '')
-                        });
-                    } else {
-                        this.setState({
-                            joinError: true,
-                            joinHelperText: "Invalid ID"
-                        });
-                    }
-                    
-            });
-    }
-
-    render() {
-        //const classes = useStyles();
-        return(
-            <React.Fragment>
-                <Typography component="h2" variant="h6" color="primary" gutterBottom>
-                    Billing Info
-                </Typography>
-                <Grid container direction="row" alignItems="center" justify="space-evenly" spacing={4} style={{height: "100%"}}>
-                    {
-                        this.state.currentProjectHardware.map(set=>{
-                            return(
-                                <Grid item alignContent="center" justify="center">
-                                    <Typography component="h2" variant="h6" color="primary" gutterBottom align="center" justify="center">
-                                        {set._id}
-                                    </Typography>
-                                    <Typography component="p" variant="h4" align="center" justify="center">
-                                        {set.cost}
-                                    </Typography>
-                                    
-                                </Grid>   
-                                 
-                            )
-                        })
-                    }
-                    <Grid item alignContent="center" justify="center">
-                        <Typography component="h2" variant="h6" color="primary" gutterBottom align="center" justify="center">
-                            +
-                        </Typography>
-                    </Grid>
-                    <Grid item alignContent="center" justify="center">
-                        <Typography component="h2" variant="h6" color="primary" gutterBottom align="center" justify="center">
-                            HWSet2
-                        </Typography>
-                        <Typography component="p" variant="h4" align="center" justify="center">
-                            20hrs
-                        </Typography>
-                        <Typography color="textSecondary" align="center" justify="center" style={{flex: 1}}>
-                            2cr/hr
-                        </Typography>
-                    </Grid>
-                    <Grid item alignContent="center" justify="center">
-                        <Typography component="h2" variant="h6" color="primary" gutterBottom align="center" justify="center">
-                            =
-                        </Typography>
-                    </Grid>
-                    <Grid item alignContent="center" justify="center">
-                        <Typography component="h2" variant="h6" color="primary" gutterBottom align="center" justify="center">
-                            Total
-                        </Typography>
-                        <Typography component="p" variant="h4" align="center" justify="center">
-                            85cr
-                        </Typography>
-                        <Typography color="textSecondary" align="center" justify="center" style={{flex: 1}}>
-                            Balance: 100cr
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </React.Fragment>
-        );
-    }
-    
-}
->>>>>>> 818812ec2db88f28e8e8d7bbdb0869342307964d
